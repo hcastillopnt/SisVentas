0de29789cb1c92ff51eb4a83d9f414306bb3ddfa
Horacio Enrique Castillo Puente <ing.hcastillo@outlook.com>
2019-01-22 18:56:12 -0600

    Se crearon los formularios y reportes, que seran utilizados en la aplicacion del UIII.RA1 - Manipulacion de Datos, aplicando la programacion por capas y el uso del ORM de Entity Framework

---
+++ SisVentas/SisVentas.WinForm/frmVistaProveedor_Ingreso.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SisVentas.WinForm
+{
+    public partial class frmVistaProveedor_Ingreso : Form
+    {
+        public frmVistaProveedor_Ingreso()
+        {
+            InitializeComponent();
+        }
+    }
+}
