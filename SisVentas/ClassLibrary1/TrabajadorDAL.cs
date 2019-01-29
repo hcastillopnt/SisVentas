6b00f7ef09c9096ae2cbbf3f52effdc4582fac4c
jealgova17 <301710383@bis.ute.edu.mx>
2019-01-29 12:13:35 -0600

    Clase para guardar los DAL ahi

---
+++ SisVentas/ClassLibrary1/TrabajadorDAL.cs
@@ -0,0 +1,25 @@
+﻿using SistemasVentas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+   public class TrabajadorDAL
+    {
+        public static SistemaDBContext dbCtx = new SistemaDBContext();
+
+        public static List<Trabajador> getTrabajadorByID(int TrabajadorID)
+        {
+            List<Trabajador> trabajadors = new List<Trabajador>();
+            //SELECT * FROM STUDENT WHERE  Id= '____'
+            trabajadors = dbCtx.Trabajadors.Where(x => x.Id == TrabajadorID).ToList();
+
+            return Trabajador;
+        }
+
+
+    }
+}
