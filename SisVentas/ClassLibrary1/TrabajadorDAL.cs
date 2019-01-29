a55acdda44e5ce560134e1d4703897efebfcfbc7
jealgova17 <301710383@bis.ute.edu.mx>
2019-01-29 12:21:23 -0600

    se agrego la clase dal

--- SisVentas/ClassLibrary1/TrabajadorDAL.cs
+++ SisVentas/ClassLibrary1/TrabajadorDAL.cs
@@ -17,8 +17,28 @@ namespace ClassLibrary1
             //SELECT * FROM STUDENT WHERE  Id= '____'
             trabajadors = dbCtx.Trabajadors.Where(x => x.Id == TrabajadorID).ToList();
 
-            return Trabajador;
+            return trabajadors;
         }
+
+        public static List<Trabajador> getAllTrabajador()
+        {
+            List<Trabajador> trabajadors = new List<Trabajador>();
+
+            //SELECT * FROM STUDENTS
+            trabajadors = dbCtx.Trabajadors.ToList();
+
+            return trabajadors
+;
+        }
+
+        public static List<Trabajador> getTrabajadorByLastName(string apellidos)
+        {
+            List<Trabajador> trabajadors = new List<Trabajador>();
+            //SELECT * FROM STUDENT WHERE  LastName= '____'
+            trabajadors = dbCtx.Trabajadors.Where(x => x.Apellidos == apellidos).ToList();
+            return trabajadors;
+        }
+
 
 
     }
