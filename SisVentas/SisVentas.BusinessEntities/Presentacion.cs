488d044fc269997481f3a7a74f99850bce5c3756
jmartinezp98 <301710151@bis.ute.edu.mx>
2019-01-29 09:42:06 -0600

    DataAnnotations de Presentacion

--- SisVentas/SisVentas.BusinessEntities/Presentacion.cs
+++ SisVentas/SisVentas.BusinessEntities/Presentacion.cs
@@ -1,18 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ComponentModel.DataAnnotations;
 
 namespace SisVentas.BusinessEntities
 {
     public class Presentacion
-    {
-        public int ID { get; set; }
 
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(50, ErrorMessage = "El nombre tiene capasidad de 50 caracteres")]
         public string Nombre { get; set; }
 
+        [Required(ErrorMessage = "La descripcion es obligatoria")]
+        [StringLength(50, ErrorMessage = "La descripcion es de 50 caracteres")]
         public string Descripcion { get; set; }
     }
 }
