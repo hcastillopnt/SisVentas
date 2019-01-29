da3f7d67c104bcfd4882071a9e950f14bc8b8afb
SaraHdzGarcia <301610199@bis.ute.edu.mx>
2019-01-29 12:40:39 -0600

    Se creo clase DAL (categoria). Se agregaron referencias en el BLL

---
+++ SisVentas/SisVentas.BusinessLogicLayer/CategoriaBLL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessEntities;
+using System.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace SisVentas.BusinessLogicLayer
+{
+    public class CategoriaBLL
+    {
+
+    }
+}
