2d054cffae8faccb9a5555c58151027e09dd929e
SaraHdzGarcia <301610199@bis.ute.edu.mx>
2019-01-29 10:07:50 -0600

    Se agrego la referencia de BusinessEntities a la libreria de DAL

---
+++ SisVentas/SisVentas.DataAccessLayer/CategoriaDAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessEntities;
+
+namespace SisVentas.DataAccessLayer
+{
+    public class CategoriaDAL
+    {
+        private static SisVentasDbContext dbCtx = new SisVentasDbContext();
+        #region INSERTAR
+        public static string insertCategoria(Categoria categoria)
+        {
+
+        }
+        #endregion
+    }
+}
