5bd88a9f1c57709b94ff045fe384a59fd190f28e
Guilleeee <301710517@bis.ute.edu.mx>
2019-01-29 10:43:40 -0600

    se agregaron campos a la clase Presentacion.

--- SisVentas/BusinessEntities/Presentacion.cs
+++ SisVentas/BusinessEntities/Presentacion.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BusinessEntities
 {
-    class Presentacion
+    public class Presentacion
     {
+        [Key]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
+        [StringLength(35, ErrorMessage = "La longuitud es de 35 caracteres")]
+        public string nombre { get; set; }
+
+        [Required(ErrorMessage = "El campo Descripcion es obligatorio")]
+        [StringLength(70, ErrorMessage = "La longuitud es de 70 caracteres")]
+        public string descripcion { get; set; }
 
     }
 }
