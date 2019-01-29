da30a4eb6dd5b99ca7377732435239f8d250888d
Guilleeee <301710517@bis.ute.edu.mx>
2019-01-29 12:32:27 -0600

    ...

--- SisVentas/SisVentas.DataAccessLayer/CategoriaDAL.cs
+++ SisVentas/SisVentas.DataAccessLayer/CategoriaDAL.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿using BusinessEntities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BusinessEntities;
 
 namespace SisVentas.DataAccessLayer
 {
