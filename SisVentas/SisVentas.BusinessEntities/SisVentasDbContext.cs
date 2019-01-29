2a0d787f77181fbd3dfec473a9db33a1872da591
Horacio Enrique Castillo Puente <ing.hcastillo@outlook.com>
2019-01-29 11:06:27 -0600

    Se creo la clase DbContext para poder manipular las entidades de la base de datos por medio de Entity Framework

---
+++ SisVentas/SisVentas.BusinessEntities/SisVentasDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SisVentas.BusinessEntities
+{
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+    public class SisVentasDbContext : DbContext
+    {
+        public SisVentasDbContext():
+            base("SisVentasDbContext")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
