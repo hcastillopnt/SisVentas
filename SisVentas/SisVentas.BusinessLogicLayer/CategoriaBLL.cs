691728ea64c2d5a104eb12bb80d7e0efd1830a40
Omarcin Espinoza <omarcin_luscius@hotmail.com>
2019-01-29 12:56:10 -0600

    Se agrego clase CateoriaBLL

--- SisVentas/SisVentas.BusinessLogicLayer/CategoriaBLL.cs
+++ SisVentas/SisVentas.BusinessLogicLayer/CategoriaBLL.cs
@@ -11,6 +11,81 @@ namespace SisVentas.BusinessLogicLayer
 {
     public class CategoriaBLL
     {
+        public static List<Categoria> getCategoriaByFilter(string Filter, string bandera)
+        {
+            //Lista para almacenar el objeto a buscar
+            List<Categoria> categorias = new List<Categoria>();
 
+            switch (bandera)
+            {
+                case "nombre":
+                    break;
+                case "descripcion":
+                    categorias = DataAccessLayer.CategoriaDAL.getCategoriaByName(Filter);
+                    break;
+                
+
+            }
+            return categorias;
+
+        }
+
+        
+
+        public static List<Categoria> getAllCategoria()
+        {
+            //Lista para almacenar el objeto a buscar
+            List<Categoria> categorias = new List<Categoria>();
+
+            //Puente entre el DataAccessLayer y el BussinesLogicLayer
+            categorias = DataAccessLayer.CategoriaDAL.getAllCategoria();
+
+            return categorias;
+        }
+
+
+                //metodo para insertar categorias
+        public static string insertCategoria(Categoria entity)
+        {
+            //Variable para almacenar el mensaje de error en caso de que ocurra alguno
+            string message = string.Empty;
+
+            //primera validacion - Verificar los campos vacios
+            if (string.IsNullOrEmpty(entity.nombre))
+            {
+                message = "El campo Nombre esta vacio, favor de completarlo";
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(entity.descripcion))
+                {
+                    message = "El campo Descripcion esta vacio, favor de completarlo";
+                }
+                else
+                {
+                  
+
+                    //Este es el puent entre la capa dde negocios y el acceso a datos
+                    message = DataAccessLayer.CategoriaDAL.insertCategoria(entity);
+
+                }
+            }
+
+
+            //regresa el mensaje con o sin errores
+            return message;
+
+        }
+        public static string removeCategoria(int id)
+        {
+            if (id > 0)
+            {
+                return DataAccessLayer.CategoriaDAL.removeCategoria(id);
+            }
+            else
+            {
+                return "Error";
+            }
+        }
     }
 }
