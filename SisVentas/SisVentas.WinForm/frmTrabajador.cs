7857309118c9f5543a9e7649a8ec0e6b72391cde
YuliannaIbco <301710084@bis.ute.edu.mx>
2019-01-29 14:20:10 -0600

    Formulario trabajador // Add

--- SisVentas/SisVentas.WinForm/frmTrabajador.cs
+++ SisVentas/SisVentas.WinForm/frmTrabajador.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BusinessEntities;
 
 namespace SisVentas.WinForm
 {
@@ -16,5 +17,43 @@ namespace SisVentas.WinForm
         {
             InitializeComponent();
         }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            //Crea la instancia del objeto a viajar
+            Trabajador trabajador = new Trabajador();
+
+            //Declaramos variables y las igualamos a las cajas de texto
+            string codigo = txtIdtrabajador.Text.Trim().ToString();
+            string nombre = txtNombre.Text.Trim().ToString();
+            string apellidos = txtApellidos.Text.Trim().ToString();
+            string sexo = cbSexo.Text;
+            DateTime fecha_nacimiento = Convert.ToDateTime(dtFecha_Nacimiento.Text);
+            string dni = txtNum_Documento.Text.Trim().ToString();
+            string direccion = txtDireccion.Text.Trim().ToString();
+            string telefono = txtTelefono.Text.Trim().ToString();
+            
+
+
+            ////Asignamos variables
+            //instructor.FirstMidName = nombre;
+            //instructor.LastName = apellido;
+
+            //Puente entre el BusinessLogicLayer y la interfaz Grafica
+            String message = SisVentas.BusinessLogicLayer.TrabajadorBLL.insertTrabajador(trabajador);
+
+            //Es para validar si ocurrio algun error
+            if (string.IsNullOrEmpty(message))
+            {
+                //Si no hubo errores, muestra un mensaje de confirmacion
+                MessageBox.Show("El trabajador ha sido registrado correctamente");
+            }
+            else
+            {
+                MessageBox.Show(message);
+            }
+        }
+
     }
 }
+
