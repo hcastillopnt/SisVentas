27b1435102e8c4bb16f65de869aa65f6f9282544
mapache06 <301710709@bis.ute.edu.mx>
2019-01-29 12:39:04 -0600

    Se creo una clase Dal para la tabla clientes ademas se agregó los metodos getClienteByID y getAllClientes

---
+++ SisVentas/ClassLibrary1/ClienteDAL.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemasVentas.DataAccessLayer
+
+{
+    class ClienteDAL
+    {
+        //se crea una instancia de la clase DbContext para acceder a la DB
+        public static SistemaDBContext dbCtx = new SistemaDBContext();
+
+
+        public static List<Cliente> getClienteByID(int StudentID)
+        {
+            List<Cliente> clientes = new List<Cliente>();
+
+            //SELECT * FROM STUDENT WHERE  Id= '____'
+            clientes = dbCtx.Clientes.Where(x => x.Id == StudentID).ToList();
+
+            return clientes;
+        }
+
+        //Metodo para obtener todos los cientes registrados en pocas palabras select * from
+        public static List<Cliente> getAllClientes()
+        {
+            List<Cliente> clientes = new List<Cliente>();
+
+            //SELECT * FROM STUDENTS
+            clientes = dbCtx.Clientes.ToList();
+
+            return clientes;
+        }
+
+
+
+
+    }
+}
