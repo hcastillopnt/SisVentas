e64a147f0961d4c877615b1f185270ddb49fc1c6
Guilleeee <301710517@bis.ute.edu.mx>
2019-01-29 09:37:57 -0600

    Se creo la clase Presentacion.

---
+++ SisVentas/BusinessEntities/Presentacion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntities
+{
+    class Presentacion
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El campo Nombre es obligatorio")]
+        [StringLength(35, ErrorMessage = "La longuitud es de 35 caracteres")]
+        public string nombre { get; set; }
+
+        [Required(ErrorMessage = "El campo Descripcion es obligatorio")]
+        [StringLength(60, ErrorMessage = "La longuitud es de 60 caracteres")]
+        public string descripcion { get; set; }
+    }
+}
+
+
