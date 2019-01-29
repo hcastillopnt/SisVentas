319b78a6f8bd0011387292a965c8a1c39f1e9bdc
Itaty Cisneros <301710679@bis.ute.edu.mx>
2019-01-29 13:57:08 -0600

    Se agregaron los metodos CRUD

--- SisVentas/SisVentas.DataAccessLayer/ProveedorDAL.cs
+++ SisVentas/SisVentas.DataAccessLayer/ProveedorDAL.cs
@@ -14,92 +14,255 @@ namespace SisVentas.DataAccessLayer
 {
     public class ProveedorDAL
     {
-        //Método para insertar un proveedor
-        //Entidad del proyecto BusinessEntities (Proveedor) entity
+        public static SisVentasDbContext dbCtx = new SisVentasDbContext();
+
+        public static List<Proveedor> getProveedorByID(int ProveedorID)
+        {
+            List<Proveedor> proveedores = new List<Proveedor>();
+            //SELECT * FROM WHERE Id = ?
+            proveedores = dbCtx.Proveedores.Where(x => x.ID == ProveedorID).ToList();
+            return proveedores;
+        }
+
+        //Método para consultar todos los estudiantes (Select * From Students), retorna lista de estudiantes registrados
+        public static List<Proveedor> getAllProveedores()
+        {
+            List<Proveedor> proveedores = new List<Proveedor>();
+
+            //SELECT * FROM Students
+            proveedores = dbCtx.Proveedores.ToList();
+
+            return proveedores;
+        }
+
+        //Método para traer un registro especifico
+        public static List<Proveedor> getProveedorByNumDocumento(string NumDocumento)
+        {
+            List<Proveedor> proveedores = new List<Proveedor>();
+
+            //SELECT * FROM Students WHERE LastName = '_____'
+            proveedores = dbCtx.Proveedores.Where(x => x.NumDocumento == NumDocumento).ToList();
+
+            return proveedores;
+
+        }
+
+        #region Método para insertar
+        //Metodo para insertar en la tabla student
         public static string insertProveedor(Proveedor entity)
         {
-            //Variable que sirve para almacenar los errores que tuviera el programa
+            // Variable para almacenar el mensaje de error en caso de que ocurra alguno
             string message = string.Empty;
-
-            //Variable que sirve para almacenar la inserción a la base de datos 
+            //Variable para almacenar si se insertó correctamente el registro
             bool isInserted = false;
 
-            //Utilizo el espacio de nombres para poder mandar a llenar la clase DbContext
-            //anteriormente creada en el proyecto BusinessEntities, y que nos servirá para
-            //realizar las consultas a la BD
-            using (SisVentasDbContext dbCtx = new SisVentasDbContext())
+            //Se utiliza un espacio de nombre para utilizar las transacciones
+            using (var dbCtxTran = dbCtx.Database.BeginTransaction())
             {
-                //Utilizo el espacio de nombres para poder llamar a la clase DbContextTransaction
-                //la cual nos servirá para realizar las operaciones CRUD de manera mas segura, a excepcion
-                //de la parte de lectura (SELECT's)
-                using (DbContextTransaction dbCtxTran = dbCtx.Database.BeginTransaction())
+
+                try
                 {
-                    //Utilizo un try-catch para encapsular el codigo que nos puede generar
-                    //problemas en un futuro y por lo tanto nos genere excepciones que podemos controlar
-                    try
+                    // Se verifica si la base de datos existe 
+                    bool isDataBaseExist = Database.Exists(dbCtx.Database.Connection);
+
+                    //Si existe la base de datos se procede a insertar el registro
+                    if (isDataBaseExist)
                     {
-                        //Esta linea nos servira para validar la existencia de la base de datos, en 
-                        //base a la cadena de conexion creada en app.config lamada sisventasdbcontext
-                        bool isDataBaseExists = Database.Exists(dbCtx.Database.Connection);
+                        // Seañade la entidad al contexto
+                        dbCtx.Proveedores.Add(entity);
 
-                        //Si la base de datos existe, se procedera a realizar las operaciones de insercion pero en el
-                        //caso de que no fuera asi se enviara una mensaje de error al usuario
-                        if (isDataBaseExists)
+                        //Se guardan los cambios en el contesto y se verifia si se efectuó de manera correcta
+                        isInserted = dbCtx.SaveChanges() > 0;
+
+                        //Si se realizó correctamente la inserción se procede a ejecutar la transacción
+                        if (isInserted)
                         {
-                            //Esta linea sirve para rastrear la entidad de la base de datos, con la que estaos trabajando
-                            //la cual debera recibir como parametro la entidad que viene desde la 
-                            //capa de negocios(validada) y desde la interfaz de usuario(con informacio)
-                            dbCtx.Proveedores.Add(entity);
+                            //Se hace commit a la transacción
+                            dbCtxTran.Commit();
+                        }
+                        #endregion
 
-                            //
-                            isInserted = dbCtx.SaveChanges() > 0;
+                       
+                    }
+                }
 
-                            if (isInserted)
+                catch (DbEntityValidationException ex)
+                {
+                    var errorMessages = ex.EntityValidationErrors.SelectMany(x => x.ValidationErrors)
+                        .Select(x => x.ErrorMessage);
+                    var fullErrorMessage = string.Join("; ", errorMessages);
+                    var exceptionMessage = string.Concat(ex.Message, "The validation errors are: ",
+                        fullErrorMessage);
+                    message = exceptionMessage + "\n" + ex.EntityValidationErrors;
+
+                    dbCtxTran.Rollback();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    var entityobj = ex.Entries.Single().GetDatabaseValues();
+
+                    if (entityobj == null)
+                        message = "The entity being updated is already deleted by another user";
+                    else
+                        message = "The entity being update has already been updated by another user";
+
+                    dbCtxTran.Rollback();
+                }
+                catch (Exception ex)
+                {
+                    message = ex.Message;
+                    dbCtxTran.Rollback();
+                }
+            }
+            return message;
+        }
+       
+
+        #region Método para ACTUALIZAR
+        //Metodo para ACTUALIZAR en la tabla student
+        public static string updateProveedor(Proveedor entity)
+        {
+            // Variable para almacenar el mensaje de error en caso de que ocurra alguno
+            string message = string.Empty;
+            //Variable para almacenar si se insertó correctamente el registro
+            bool isUpdated = false;
+
+            //Se utiliza un espacio de nombre para utilizar las transacciones
+            using (var dbCtxTran = dbCtx.Database.BeginTransaction())
+            {
+                try
+                {
+                    // Se verifica si la base de datos existe 
+                    bool isDataBaseExist = Database.Exists(dbCtx.Database.Connection);
+
+                    //Si existe la base de datos se procede a insertar el registro
+                    if (isDataBaseExist)
+                    {
+                        //Alternativa #1 Actualizando con Entity Framework
+                        //Buscamos el objeto a actualizar
+                        var proveedor = dbCtx.Proveedores.Find(entity.ID);
+
+                        //Validar que el objeto exista
+                        if (proveedor != null)
+                        {
+                            //Damos los valores a modificar
+                            proveedor.RazonSocial = entity.RazonSocial;
+                            proveedor.SectorComercial = entity.SectorComercial;
+                            proveedor.TipoDocumento = entity.TipoDocumento;
+                            proveedor.NumDocumento = entity.NumDocumento;
+                            proveedor.Direccion = entity.Direccion;
+                            proveedor.Telefono = entity.Telefono;
+                            proveedor.Email = entity.Email;
+                            proveedor.URL = entity.URL;
+                            
+                            //Se añade la entidad al contexto
+                            dbCtx.Entry(proveedor).State = EntityState.Modified;
+
+                            //Se guardan los cambios en el contexto, y se verifica si se efectuo de manera correcta
+                            isUpdated = dbCtx.SaveChanges() > 0;
+
+                            //Si se realizó correctamente la actualización se procede a ejecutar la transacción
+                            if (isUpdated)
                             {
                                 dbCtxTran.Commit();
                             }
-                            else
-                            {
-                                message = "El registro no se efecuó correctamente";
-                            }
                         }
-                        else
-                        {
-                            message = "La base de datos a la que intenta acceder no se encuentra disponible, favor de contactar al administrador";
-                        }
-                    }
-
-                    catch (DbEntityValidationException ex)
-                    {
-                        var errorMessages = ex.EntityValidationErrors.SelectMany(x => x.ValidationErrors)
-                            .Select(x => x.ErrorMessage);
-                        var fullErrorMessage = string.Join("; ", errorMessages);
-                        var exceptionMessage = string.Concat(ex.Message, "The validation errors are: ",
-                            fullErrorMessage);
-                        message = exceptionMessage + "\n" + ex.EntityValidationErrors;
-
-                        dbCtxTran.Rollback();
-                    }
-                    catch (DbUpdateConcurrencyException ex)
-                    {
-                        var entityobj = ex.Entries.Single().GetDatabaseValues();
-
-                        if (entityobj == null)
-                            message = "The entity being updated is already deleted by another user";
-                        else
-                            message = "The entity being update has already been updated by another user";
-
-                        dbCtxTran.Rollback();
-                    }
-                    catch (Exception ex)
-                    {
-                        message = ex.Message;
-                        dbCtxTran.Rollback();
                     }
                 }
-                return message;
-            }
+                catch (DbEntityValidationException ex)
+                {
+                    var errorMessages = ex.EntityValidationErrors.SelectMany(x => x.ValidationErrors)
+                        .Select(x => x.ErrorMessage);
+                    var fullErrorMessage = string.Join("; ", errorMessages);
+                    var exceptionMessage = string.Concat(ex.Message, "The validation errors are: ",
+                        fullErrorMessage);
+                    message = exceptionMessage + "\n" + ex.EntityValidationErrors;
 
+                    dbCtxTran.Rollback();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    var entityobj = ex.Entries.Single().GetDatabaseValues();
+
+                    if (entityobj == null)
+                        message = "The entity being updated is already deleted by another user";
+                    else
+                        message = "The entity being update has already been updated by another user";
+
+                    dbCtxTran.Rollback();
+                }
+                catch (Exception ex)
+                {
+                    message = ex.Message;
+                    dbCtxTran.Rollback();
+                }
+            }
+            return message;
         }
+        #endregion
+        public static string removeProveedor(int ProveedorID)
+        {
+            //Almacena los errores del método
+            string message = string.Empty;
+            //Almacena el estatus del método
+            bool isRemoved = false;
+
+            using (var dbCtxTran = dbCtx.Database.BeginTransaction())
+            {
+                try
+                {
+                    bool isDataBaseExist = Database.Exists(dbCtx.Database.Connection);
+
+                    if (isDataBaseExist)
+                    {
+                        //Consulta para obtener el objeto a eliminar
+                        var objProveedor = dbCtx.Proveedores.Where(x => x.ID == ProveedorID)
+                            .SingleOrDefault();
+
+                        //Consulta para eliminar el objeto
+                        dbCtx.Proveedores.Remove(objProveedor);
+
+                        //Guardar el estatus del borrado
+                        isRemoved = dbCtx.SaveChanges() > 0;
+
+                        //Si se eliminó correctamente hago el commit
+                        if (isRemoved)
+                        {
+                            dbCtxTran.Commit();
+                        }
+                    }
+                }
+                catch (DbEntityValidationException ex)
+                {
+                    var errorMessages = ex.EntityValidationErrors.SelectMany(x => x.ValidationErrors)
+                        .Select(x => x.ErrorMessage);
+                    var fullErrorMessage = string.Join("; ", errorMessages);
+                    var exceptionMessage = string.Concat(ex.Message, "The validation errors are: ",
+                        fullErrorMessage);
+                    message = exceptionMessage + "\n" + ex.EntityValidationErrors;
+
+                    dbCtxTran.Rollback();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    var entityobj = ex.Entries.Single().GetDatabaseValues();
+
+                    if (entityobj == null)
+                        message = "The entity being updated is already deleted by another user";
+                    else
+                        message = "The entity being update has already been updated by another user";
+
+                    dbCtxTran.Rollback();
+                }
+                catch (Exception ex)
+                {
+                    message = ex.Message;
+                    dbCtxTran.Rollback();
+                }
+            }
+            return message;
+        }
+
     }
+    
 }
