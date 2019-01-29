881b7bdd13754d809c9dcc9715e6c9b42036074d
SaraHdzGarcia <301610199@bis.ute.edu.mx>
2019-01-29 09:24:48 -0600

    Se creo la clase Categorias en BusinessEntities

---
+++ SisVentas/BusinessEntities/Categorias.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntities
+{
+    public class Categorias
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required(ErrorMessage ="Este campo es obligatorio")]
+        [StringLength(50,ErrorMessage ="Longitud maxima de 50 caracteres")]
+        public String nombre { get; set; }
+
+        [StringLength(60,ErrorMessage ="Longitud maxima de 60 caracteres")]
+        public String descripcion { get; set; }
+    }
+}
