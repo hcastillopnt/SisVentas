af3a8905c5eca0fddcb95a87cc016f5c5a28511f
Edson Villarreal Hernandez <301710377@bis.ute.edu.mx>
2019-01-29 14:34:13 -0600

    Se modificio ventasBILL

---
+++ SisVentas/SistemasVentas.BussinesLogicLayer/VentasBILL.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemasVentas.BussinesLogicLayer
+{
+    class VentasBILL
+    {
+        public static List<Venta> getVentasByFilter(string Filter, string bandera)
+        {
+            //Lista para almacenar el objeto a buscar
+            List<Venta> ventas = new List<Venta>();
+
+            switch (bandera)
+            {
+                case "FirstMideName":
+                    break;
+                case "LastName":
+                    ventas = DataAccessLayer.VentasDAL.getVentaByID(Filter);
+                    break;
+                case "StudentId":
+                    int StudentID = Convert.ToInt32(Filter);
+                    ventas = DataAccessLayer.VentasDAL.getVentaByID(vent);
+                    break;
+
+            }
+            return ventas;
+
+        }
+        public static List<Venta> getVentaByID(int VentasId)
+        {
+            //Lista para almacenar el objeto a buscar
+            List<Venta> ventas
+                = new List<Venta>();
+
+            //Puente entre el DataAccessLayer y el BussinesLogicLayer
+            ventas = DataAccessLayer.VentasDAL.getVentaByID(VentasId);
+
+            return ventas;
+
+
+        }
+        public static List<Venta> ggetAllVentas()
+        {
+            //Lista para almacenar el objeto a buscar
+            List<Venta> ventas = new List<Venta>();
+
+            //Puente entre el DataAccessLayer y el BussinesLogicLayer
+            ventas = DataAccessLayer.VentasDAL.getAllVentas();
+
+            return ventas;
+        }
+        // Se tiene que buscar por fecha 
+        /* //metodo para obtener los estudiantes por medio del apellido
+         public static List<Venta> getStudentsByLastName(string lastname)
+         {
+             List<Student> students = new List<Student>();
+             //SELECT * FROM STUDENT WHERE  LastName= '____'
+             students = DataAccessLayer.StudentDAL.getStudentsByLastName(lastname);
+             return students;
+         }*/
+
+        //metodo para insertar los estudiantes
+        public static string insertVentas(Venta entity)
+        {
+            //Variable para almacenar el mensaje de error en caso de que ocurra alguno
+            string message = string.Empty;
+
+            //primera validacion - Verificar los campos vacios
+            if (string.IsNullOrEmpty(entity.TipoComporbante)
+            {
+                message = "El campo tipo esta vacio, favor de completarlo";
+            }
+            else
+            {
+               
+                else
+                {
+                    //Definir la fecha de enrolamiento de manera automatica
+                    entity.EnrollementDate = DateTime.Now;
+
+                    //Este es el puent entre la capa dde negocios y el acceso a datos
+                    message = DataAccessLayer.VentasDAL.insertVentas(entity);
+
+                }
+            }
+
+            //regresa el mensaje con o sin errores
+            return message;
+        }
+        public static string updateVentas(Venta entity)
+        {
+            //Variable para almacenar el mensaje de error en caso de que ocurra alguno
+            string message = string.Empty;
+
+            //primera validacion - Verificar los campos vacios
+            if (string.IsNullOrEmpty(entity.Serie))
+            {
+                message = "El campo Apellido esta vacio, favor de completarlo";
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(entity.Correlativo))
+                {
+                    message = "El campo Nombre esta vacio, favor de completarlo";
+                }
+                else
+                {
+                    //Definir la fecha de enrolamiento de manera automatica
+                    entity.EnrollementDate = DateTime.Now;
+
+                    //Este es el puent entre la capa dde negocios y el acceso a datos
+                    message = DataAccessLayer.VentasDAL.updateVenta; 
+(entity);
+
+                }
+            }
+
+
+            //regresa el mensaje con o sin errores
+            return message;
+        }
+        public static string removeVenta(int VentaId)
+        {
+            //Variable para almacenar el mensaje de error en caso de que ocurra alguno
+            string message = string.Empty;
+
+            if (VentaId
+                > 0)
+            {
+
+                return DataAccessLayer.VentasDAL.removeVenta (VentaId);
+
+            }
+            else
+            {
+                return "Error";
+            }
+
+        }
+    }
+}
+
+        
+
