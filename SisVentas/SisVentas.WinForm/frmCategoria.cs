db0b3c916e0045c9c75ed6f4a81be899589248cf
SaraHdzGarcia <301610199@bis.ute.edu.mx>
2019-01-29 17:43:19 -0600

    Formulario Trabajador

--- SisVentas/SisVentas.WinForm/frmCategoria.cs
+++ SisVentas/SisVentas.WinForm/frmCategoria.cs
@@ -25,17 +25,13 @@ namespace SisVentas.WinForm
             Categoria categoria = new Categoria();
 
             //Declaramos variables y las igualamos a las cajas de texto
-            string codigo = txtIdcategoria.Text.Trim().ToString();
             string nombre = txtNombre.Text.Trim().ToString();
             string descripcion = txtDescripcion.Text.Trim().ToString();
 
             //Asignamos variables
-            //***VERIFICAR COMO  SE ASIGNA ID
-            //categoria.id = codigo;
             categoria.nombre = nombre;
             categoria.descripcion = descripcion;
-
-            //***VERIFICAR BIEN SI ESTA CORRECTO LA LINEA DEL MESSAGE
+            
             //Puente entre el BusinessLogicLayer y la interfaz Grafica
             String message = SisVentas.BusinessLogicLayer.CategoriaBLL.insertCategoria(categoria);
 
@@ -45,11 +41,7 @@ namespace SisVentas.WinForm
                 //Si no hubo errores, muestra un mensaje de confirmacion
                 MessageBox.Show("El registro ha sido guardado correctamente");
 
-
-                //********FALTA PONER LA LINEA DEL PRECARGADO******
-                //Precargado
-                //dgvStudent.DataSource = DesApl.BusinessLogicLayer.StudentBLL.getAllStudent();
-                //limpiar();
+                dataListado.DataSource = SisVentas.BusinessLogicLayer.CategoriaBLL.getAllCategoria();
             }
             else
             {
