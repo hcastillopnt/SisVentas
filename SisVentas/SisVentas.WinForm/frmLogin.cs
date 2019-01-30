83ac45261254c233690b7e973d02255262edd560
yeseniamlr <301710515@bis.ute.edu.mx>
2019-01-29 19:24:39 -0600

    Se agrego Login

--- SisVentas/SisVentas.WinForm/frmLogin.cs
+++ SisVentas/SisVentas.WinForm/frmLogin.cs
@@ -16,5 +16,49 @@ namespace SisVentas.WinForm
         {
             InitializeComponent();
         }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnIngresar_Click(object sender, EventArgs e)
+        {
+            if ((TxtUsuario.Text != "") && (TxtPassword.Text != ""))
+            {
+                if ((TxtUsuario.Text.Equals("guille")) && (TxtPassword.Text.Equals("guille123")))
+                {
+                    frmPrincipal principal = new frmPrincipal();
+                    principal.Show();
+                    this.Hide(); //ocultar formulario con el que se esta trabajando ej."Login" 
+                }
+                else
+                {
+                    MessageBox.Show("Usuario y/o Contraseña son incorrectas",
+                                    "Login",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+
+                    TxtUsuario.Clear();
+                    TxtPassword.Clear();
+                    TxtPassword.Focus();
+                    TxtUsuario.Focus();
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Esbribe el usuario y contraseña",
+                                 "Login",
+                                 MessageBoxButtons.OK,
+                                 MessageBoxIcon.Exclamation);
+                TxtUsuario.Focus();
+            }
+        }
+
+        private void BtnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit(); 
+        }
     }
 }
